2b12accfbc45f7dbcd503907e9dded64dda2805b
alexmcode <musca.alexandru@gmail.com>
2017-01-25 15:28:06 +0200

    Added ngFor directive to generate dinamically the list of recipes using as an input a static array. Created ingredient class to be part of a recipe.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -6,9 +6,11 @@ import {Recipe} from "../recipe";
   templateUrl: './recipe-list.component.html'
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [];
+  recipes: Recipe[] = [
+    new Recipe('Schnitzel', 'Very tasty', 'http://www.kitchenproject.com/german/recipes/Schnitzel/SchnitzelPreparation/pictures/IMG_4527.JPG', []),
+    new Recipe('Summer Salad', 'Okayish', 'http://ohmyveggies.com/wp-content/uploads/2013/06/the_perfect_summer_salad.jpg', [])
+  ];
   @Output() recipeSelected = new EventEmitter<Recipe>();
-  recipe = new Recipe('Dummy', 'Dummy', 'http://thumbs2.ebaystatic.com/d/l225/m/mfXELL6zPWJE4OC0agiXMZw.jpg')
 
   constructor() { }
 
